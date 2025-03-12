0286996ec2fceecdb1329befe57980a47e7f0d29
Mark H <pub@bivens.dev>
2025-03-12 19:44:41 +0000

    Add documentation for dataset class

--- lib/src/dataset.dart
+++ lib/src/dataset.dart
@@ -1,14 +1,72 @@
 import 'package:rdf_dart/src/graph.dart';
 import 'package:rdf_dart/src/iri.dart';
 
+/// Represents an RDF Dataset, which is a collection of RDF graphs.
+///
+/// An RDF Dataset consists of a default graph and zero or more named graphs.
+/// The default graph is an unnamed graph that contains a set of triples.
+/// Named graphs are graphs that are associated with an IRI (Internationalized
+/// Resource Identifier). Each named graph contains a set of triples and can be
+/// accessed by its associated IRI.
+///
+/// This class provides methods to manage the default graph and named graphs within
+/// the dataset.
 class Dataset {
-  final Graph defaultGraph = Graph();
-  final Map<IRI, Graph> namedGraphs = {};
+  /// The default graph of the dataset.
+  ///
+  /// This graph contains a set of triples that are not associated with any
+  /// named graph.
+  final Graph defaultGraph;
 
+  /// The named graphs of the dataset.
+  ///
+  /// This map associates an [IRI] with a [Graph], where the IRI is the name
+  /// of the graph and the Graph is the set of triples associated with that
+  /// name.
+  final Map<IRI, Graph> namedGraphs;
+
+  /// Creates a new empty Dataset.
+  ///
+  /// The dataset is initialized with an empty [defaultGraph] and an empty
+  /// set of [namedGraphs].
+  Dataset()
+      : defaultGraph = Graph(),
+        namedGraphs = {};
+
+  /// Adds a named graph to the dataset.
+  ///
+  /// The [name] is the IRI that identifies the named graph, and [graph] is the
+  /// graph to add to the dataset.
+  ///
+  /// If a named graph with the same [name] already exists, it will be
+  /// replaced by the new graph.
+  ///
+  /// Example:
+  /// ```dart
+  /// final dataset = Dataset();
+  /// final graph = Graph();
+  /// final name = IRI('http://example.com/graph');
+  /// dataset.addNamedGraph(name, graph);
+  /// ```
   void addNamedGraph(IRI name, Graph graph) {
     namedGraphs[name] = graph;
   }
 
+  /// Removes a named graph from the dataset.
+  ///
+  /// The [name] is the IRI that identifies the named graph to remove.
+  ///
+  /// If no named graph with the given [name] exists, this method does nothing.
+  ///
+  /// Example:
+  /// ```dart
+  /// final dataset = Dataset();
+  /// final name = IRI('http://example.com/graph');
+  /// // Add the graph first
+  /// dataset.addNamedGraph(name, Graph());
+  /// // remove the graph
+  /// dataset.removeNamedGraph(name);
+  /// ```
   void removeNamedGraph(IRI name) {
     namedGraphs.remove(name);
   }
