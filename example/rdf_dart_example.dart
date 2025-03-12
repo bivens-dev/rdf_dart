9e0ddfb47ac8183c23e3e9cb703beab4612d5378
Mark H <pub@bivens.dev>
2025-03-12 21:07:50 +0000

    Update example

--- example/rdf_dart_example.dart
+++ example/rdf_dart_example.dart
@@ -1,9 +1,24 @@
 import 'package:rdf_dart/rdf_dart.dart';
 
 void main() {
-  var iri = IRI('http://example.com /path');
-  
-  print('url: $iri');
+  // Create an IRI
+  final subject = IRI('http://example.com/resource');
 
-  print(IRI('http://www.example.com/r\u00e9sum\u00e9'));
+  // Create an IRI
+  final perdicate = IRI('http://example.com/predicate');
+
+  // Create a Literal
+  final object = Literal(
+    'Hello',
+    IRI('http://www.w3.org/2001/XMLSchema#string'),
+  );
+
+  // Create a Triple
+  final triple = Triple(subject, perdicate, object);
+
+  // Create a Dataset
+  final dataset = Dataset();
+
+  // Add the Triple to the Dataset
+  dataset.defaultGraph.add(triple);
 }
